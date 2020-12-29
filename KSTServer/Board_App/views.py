fdf0f7f177c940d07028acbf8f0e3a6e1b76bd0d
brown2243 <brown2243@naver.com>
2020-12-29 21:05:28 +0900

    server키면 react화면 나오게 구현

--- KSTServer/Board_App/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http imort HttpResponse
-
-# Create your views here.
-def index(request):
-	return render(request, 'Board_App/index.html')
--- server/config/urls.py
+++ server/config/urls.py
@@ -1,4 +1,4 @@
-"""KSTServer URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -19,5 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
